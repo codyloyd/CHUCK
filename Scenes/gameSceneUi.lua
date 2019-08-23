730fb2fd345d93bf86491bc4219f27fee50cf488
Cody Loyd <codyloyd@gmail.com>
2019-08-23 07:46:04 -0500

    tweak ui

--- Scenes/gameSceneUi.lua
+++ Scenes/gameSceneUi.lua
@@ -15,7 +15,10 @@ function hud.new(uiStack)
   end
 
   function self:draw()
-    love.graphics.print( "Press 'i' for inventory", 22, 88 )
+    love.graphics.scale(.5,.5)
+    love.graphics.print( "Press 'i' for inventory", 16, 16 )
+    love.graphics.print( "Press 'p' to suicide", 16, 32 )
+    love.graphics.print( "Press 'esc' for DEBUG MODE", 16, 48 )
   end
 
   function self:keypressed(key)
