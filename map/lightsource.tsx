19136b102acb3cde56403e2d3994cf1c82c0ed55
Cody Loyd <codyloyd@gmail.com>
2019-08-30 11:32:23 -0500

    add light source

---
+++ map/lightsource.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lightsource" tilewidth="400" tileheight="400" tilecount="1" columns="1">
+ <image source="../assets/lightsource.png" width="400" height="400"/>
+</tileset>
