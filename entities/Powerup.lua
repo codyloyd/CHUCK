35727d35c0131a65361e58cbacc9efc11f450c06
Briggs Elsperger <briggse@protonmail.com>
2019-08-31 17:01:30 -0500

    adds powerup sprite

--- entities/Powerup.lua
+++ entities/Powerup.lua
@@ -6,20 +6,32 @@ local Powerup = class("Powerup", Entity)
 function Powerup:initialize(opts, world)
   Entity.initialize(self, opts)
 
-  self.w = 5
-  self.h = 5
+  self.w = 15
+  self.h = 15
   self.causesDamage = false
   self.name=opts.name
+
+  -- Animation
+  self.originX = opts.x
+  self.originY = opts.y
+  self.spritesheet=love.graphics.newImage('assets/POWERUP.png')
+  self.movingUp=true
 
   world:add(self, self.x, self.y, self.w, self.h)
 end
 
 function Powerup:update(dt)
-  return
+  if self.movingUp then
+    self.y = self.y - 0.6 * dt
+    if self.y < self.originY - 0.8 then self.movingUp = false end
+  else
+    self.y = self.y + 0.6 * dt
+    if self.y > self.originY then self.movingUp = true end
+  end
 end
 
 function Powerup:draw()
-  love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+  love.graphics.draw(self.spritesheet, self.x, self.y, 0)
 end
 
 function Powerup:collected()
