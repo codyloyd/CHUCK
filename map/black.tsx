571ba6af202df29d57c2302fde34fed6736afda6
Cody Loyd <codyloyd@gmail.com>
2019-09-09 20:15:49 -0500

    some tiles

---
+++ map/black.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="black" tilewidth="100" tileheight="100" tilecount="1" columns="1">
+ <image source="../assets/black.png" width="100" height="100"/>
+</tileset>
---
+++ map/fadetoblack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fadetoblack" tilewidth="100" tileheight="100" tilecount="1" columns="1">
+ <image source="../assets/fadetoblack.png" width="100" height="100"/>
+</tileset>
